8b930bb40bb6481c8c77343c7e16da27f714a68d
faithhernandez <58082665+faithhernandez@users.noreply.github.com>
2019-11-22 11:11:20 -0500

    Add files via upload

---
+++ HernandezFMadlibs.py
@@ -0,0 +1,14 @@
+word = input("Where is your favorite place to visit?")
+
+word2 = input("How many siblings do you have?")
+
+word3 = input("What is your favorite animal?")
+
+word4 = input("Who is your favorite celebrity?")
+
+word5 = input("Pick a number from 1-100")
+
+print("For christmas I went to" + word + "with my" + word2 + "brothers. In" + word + "we saw" + word3 + "and we even saw" + word4 + "They were surrounded by" + word5 + "screaming fans.")
+              
+
+
